2d38c106d772f18e951177f81d87f97e5b8d2a0f
Indra Gunawan <indra@propertyguru.com.sg>
2021-06-18 15:17:57 +0700

    debug dry-run

--- rabbitmq/files/shovel.sh
+++ rabbitmq/files/shovel.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 ## Create shovel users-forward-converter-delta
 
-rabbitmqctl -p users-api set_parameter shovel users-forward-converter-delta  '{"src-protocol": "amqp091", "src-uri": "amqp://usersapi@/users-api", "src-exchange": "users-api", "src-exchange-key": "user.update.delta", "dest-protocol": "amqp091", "dest-uri": "amqp://data-bridge@/data-bridge", "dest-exchange": "forward-converter", "ack-mode": "on-confirm","src-delete-after": "never"}' {%- if dry_run == "False" %}--dry-run{%- endif %}
+#rabbitmqctl -p users-api set_parameter shovel users-forward-converter-delta  '{"src-protocol": "amqp091", "src-uri": "amqp://usersapi@/users-api", "src-exchange": "users-api", "src-exchange-key": "user.update.delta", "dest-protocol": "amqp091", "dest-uri": "amqp://data-bridge@/data-bridge", "dest-exchange": "forward-converter", "ack-mode": "on-confirm","src-delete-after": "never"}' {%- if dry_run == "False" %}--dry-run{%- endif %}
+
+echo "{{ dry_run }}"
